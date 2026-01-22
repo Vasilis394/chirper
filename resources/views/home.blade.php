525bf05942167303569a4707107554a310422d50
Vasilis394 <waka4business@gmail.com>
2026-01-22 15:06:20 +0100

    rewrite layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,9 @@
         Welcome
     </x-slot:title>
      <div class="max-w-2xl mx-auto">
+        @foreach ($chirps as $chirp)
+            
+        
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
@@ -12,5 +15,6 @@
                     </div>
                 </div>
             </div>
+        @endforeach    
         </div>
 </x-layout>
